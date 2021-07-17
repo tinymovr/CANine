31e404abc91a24deecbb1bd91a9d167f0c78ceb8
Yannis <yconst@gmail.com>
2021-07-17 14:40:08 +0300

    Update flash.bat

--- flash.bat
+++ flash.bat
@@ -15,14 +15,14 @@ if exist dfu-util-static.exe (
     rem DFU-util exists, skipping download
 ) else (
     rem Downloading DFU-util
-    curl.exe --output dfu-util-static.exe --url https://github.com/yconst/CANine/releases/latest/dfu-util-static.exe)
+    curl.exe --output dfu-util-static.exe --url https://github.com/yconst/CANine/releases/latest/download/dfu-util-static.exe)
 )
 
 if exist CANine.bin (
     rem Firmware exists, skipping download
 ) else (
     rem Downloading firmware
-    curl.exe --output CANine.bin --url https://github.com/yconst/CANine/releases/latest/CANine.bin)
+    curl.exe --output CANine.bin --url https://github.com/yconst/CANine/releases/latest/download/CANine.bin)
 )
 
 dfu-util-static.exe -c 1 -i 0 -a 0 -s 0x08000000 -D CANine.bin
